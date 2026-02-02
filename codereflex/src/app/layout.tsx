f0e7e5a40067509a673d942056310b9ac47ac584
Shubh Bajpai <sbaj0015@student.monash.edu>
2026-02-02 14:34:46 +1100

    wrapping error fixed

--- codereflex/src/app/layout.tsx
+++ codereflex/src/app/layout.tsx
@@ -30,8 +30,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
-        {children}
-        <Analytics />
+        <TRPCReactProvider>
+          {children}
+          <Analytics />
+        </TRPCReactProvider>
       </body>
     </html>
   );
